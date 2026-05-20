b6f6a15469577d21a32880cc5cd5c271a58f1be8
RedFox65 <cle.coirier@gmail.com>
2026-05-20 21:53:44 +0200

    Ajout de la section des compétences et amélioration de la section 'à propos'

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  // =========================
+  // SECTION A PROPOS
+  // =========================
+
   const tabs = document.querySelectorAll("#a-propos li");
   const articles = document.querySelectorAll("#a-propos article");
 
@@ -21,4 +26,100 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+
+  // =========================
+  // SECTION COMPETENCES
+  // =========================
+
+  const progressBars = document.querySelectorAll(".progress-bar");
+
+  progressBars.forEach(bar => {
+
+    const text = bar.querySelector("p");
+
+    // texte original
+    const originalText = text.textContent;
+
+    // texte affiché au hover
+    let hoverText = "";
+
+    switch(originalText.trim()) {
+
+      case "C":
+        hoverText = "75%";
+        break;
+
+      case "Python":
+        hoverText = "90%";
+        break;
+
+      case "Java":
+        hoverText = "85%";
+        break;
+
+      case "Bash":
+        hoverText = "70%";
+        break;
+
+      case "HTML / CSS":
+        hoverText = "75%";
+        break;
+
+      case "PHP":
+        hoverText = "30%";
+        break;
+
+      case "JavaScript":
+        hoverText = "10%";
+        break;
+
+      case "MySQL":
+        hoverText = "45%";
+        break;
+
+      case "pgSQL":
+        hoverText = "45%";
+        break;
+
+      case "Tutorial D":
+        hoverText = "20%";
+        break;
+
+      case "UML":
+        hoverText = "65%";
+        break;
+
+      case "Git":
+        hoverText = "70%";
+        break;
+
+      case "Figma":
+        hoverText = "80%";
+        break;
+
+      case "Trello":
+        hoverText = "95%";
+        break;
+
+      case "Project libre":
+        hoverText = "55%";
+        break;
+
+      default:
+        hoverText = originalText;
+    }
+
+    // souris entre
+    bar.addEventListener("mouseenter", () => {
+      text.textContent = hoverText;
+    });
+
+    // souris sort
+    bar.addEventListener("mouseleave", () => {
+      text.textContent = originalText;
+    });
+
+  });
+
 });
